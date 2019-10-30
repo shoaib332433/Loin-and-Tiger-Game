02927ac0c2990f551231960cf2fc27ac8eb95b5d
shoaib332433 <shoaib.shah660@gmail.com>
2019-10-30 15:51:29 +0500

    Ui Part Completed

---
+++ app/src/main/java/com/example/lionandtigergame/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lionandtigergame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
